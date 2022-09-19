959fe08723cdaae352a8fbdb594bb182a6aaa7ca
Leonardo Nunes <itsmeleo_n@outlook.com>
2022-09-19 17:07:05 -0300

    search bonus done

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 
+//importando cors
+const cors = require("cors")
+
 const PORT = 4000;
 
 // Importing all the pokemon for our data file
 let allPokemon = require("./data");
 
+//instanciando o express na variável APP
 const app = express();
+app.use(cors({origin: "http://localhost:3000"})) // se colocar origin: "*" quer dizer que vai aceitar TODAS as requisições, isso não é legal
 
+
+//configurando o nosso servidor para receber e enviar arquivos em JSON
 app.use(express.json());
 
 // -- Define your route listeners here! --
@@ -70,6 +77,38 @@ app.delete("/allPokemon/delete/:id", (req, res) => {
     allPokemon = newArray  
 
     return res.status(200).json(allPokemon)
+
+})
+
+
+//buscar um pokemon por NOME
+app.get("/search", (req, res, next) => {
+
+    let foundPokemon = []
+
+    if (req.query.name) {
+        console.log("sim, estão procurando por NOME")
+
+        allPokemon.forEach((pokemon) => {
+            if (pokemon.name === req.query.name) {
+                foundPokemon.push(pokemon)
+            }
+
+        })
+        return res.status(200).json(foundPokemon)
+    }
+    
+    if (req.query.types) {
+        console.log("sim, estão procurando por TYPE")
+
+        allPokemon.forEach((pokemon) => {
+            if (pokemon.types.includes(req.query.types)) {
+                foundPokemon.push(pokemon)
+            }
+
+        })
+        return res.status(200).json(foundPokemon)
+    }
 
 })
 
